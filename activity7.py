d02d38764a368dd86e4c6633750692acdc84e494
jem <jemaikaondon04@gmail.com>
2026-09-08 11:32:02 +0800

    67

---
+++ activity7.py
@@ -0,0 +1,36 @@
+total = 0
+
+no1 = eval(input('Enter a number: '))
+total += no1
+
+no2 = eval(input('Enter a number: '))
+total -= no2
+
+no3 = eval(input('Enter a number: '))
+total *= no3
+
+no4 = eval(input('Enter a number: '))
+total /= no4
+
+no5 = eval(input('Enter a number: '))
+total //= no5
+
+no6 = eval(input('Enter a number: '))
+total %= no6
+
+no7 = eval(input('Enter a number: '))
+total -= no7
+
+no8 = eval(input('Enter a number: '))
+total *= no8
+
+no9 = eval(input('Enter a number: '))
+total /= no9
+
+no10 = eval(input('Enter a number: '))
+total //= no10
+
+
+
+
+print('The sum is -> ', total)
---
+++ activity8.py
@@ -0,0 +1,18 @@
+hobbies = '' # blank string
+
+hob = input('Ano hobbies mo? ')
+hob1 = input('May iba ka pa bang hobbies? ')
+
+hobbies += hob + ', '
+hobbies += 'and ' + hob1
+
+print('So, my hobbies are ', hobbies)
+
+favorite = ''
+fav = input('Favorites food mo? ')
+fav1 = input('Baka may ibang favorite food ka pa? ')
+
+favorite += fav + ', '
+favorite += 'and ' + fav1
+
+print('So, my favorite food are ', favorite)
