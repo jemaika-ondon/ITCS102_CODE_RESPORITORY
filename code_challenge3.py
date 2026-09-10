755f9123f66bcb19e58b1a7b28bcf238748d87c6
jem <jemaikaondon04@gmail.com>
2026-09-10 21:56:47 +0800

    09-10

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,24 +1,30 @@
-# Problem: Global Freight Calculator  
+# Problem: Global Freight Calculator 
 
-sender_name = input('Enter Your Name: ')
-type_of_item = input('Enter Your Item: ') 
-is_Fragile = bool(input('Is it FRAGILE? '))
-weight = eval(input('Your Items Weight?' ))
-distance = eval(input('Gaano Kalayo?' ))
-is_express = bool(input('Nagmamadali kaba? '))
-is_international = bool(input('International ba? '))
+sender_name = input('Enter Your NAME: ')
+type_of_item = input('Enter Your ITEM: ')
+is_Fragile = input('Is the item FRAGILE? (True/False): ') == 'True'
+weight = eval(input('How much does it WEIGHT? (kg): '))
+distance = eval(input('How far is the DESTINATION? (km): '))
+is_express = input('Do you want EXPRESS delivery? (True/False): ') == 'True'
+is_international = input('Is this an INTERNATIONAL delivery? (True/False): ') == 'True'
 
 base_cost = (weight * 2.50) + (distance * 0.15)
 
-#print(base_cost)
-#print(sender_name )
-#print(type_of_item)
-#print(is_Fragile)
-#print(weight)
-#print(distance )
-#print(is_express)
-#print( is_international)
+if weight <= 2 and distance <= 100 and is_express == False and is_international == False :
+    print('Congratulations you got FREE SHIPPING!!')
 
-if (weight <= 2.0 and distance <= 100) and (is_express == False and is_: 
-    print('Free Shipping')
-elif 
+elif is_international and is_express == True:
+    total = (base_cost * 1.40) + 50
+    print('International Express Fee:$',total)
+
+elif is_express == True or (is_international == True and weight > 20):
+    total = (base_cost * 1.20) + 25
+    print('Express or Heavy International Fee:$ ',total)
+
+elif weight > 30 or distance > 1000:
+    total = base_cost + 30 
+    print('Oversized Fee:$',total)
+
+else:
+    total = base_cost
+    print('Standard Rate:$',total)
